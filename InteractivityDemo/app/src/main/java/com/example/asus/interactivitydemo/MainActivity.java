20a37842d3df4f49cb63181aad2c167dfed2689b
SwapKul <47084735+SwapKul@users.noreply.github.com>
2020-07-24 10:37:33 +0530

    updates

--- InteractivityDemo/app/src/main/java/com/example/asus/interactivitydemo/MainActivity.java
+++ InteractivityDemo/app/src/main/java/com/example/asus/interactivitydemo/MainActivity.java
@@ -4,12 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View view){
+    public void login(View view){
 
-        Log.i("Info", "It Worked!");
+        EditText userName = (EditText) findViewById(R.id.userName);
+
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        Log.i("Info", "Button Pressed");
+
+        Log.i("Username", userName.getText().toString());
+
+        Log.i("Password", passwordText.getText().toString());
+
+        Toast.makeText(this, "Hi there", Toast.LENGTH_SHORT).show();
 
     }
 
