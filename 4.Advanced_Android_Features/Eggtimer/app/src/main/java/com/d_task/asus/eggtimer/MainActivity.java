221e004b7e5de9271fb4b87c92e5893996b2184e
SwapKul <47084735+SwapKul@users.noreply.github.com>
2020-08-23 15:55:33 +0530

    updates

--- 4.Advanced_Android_Features/Eggtimer/app/src/main/java/com/d_task/asus/eggtimer/MainActivity.java
+++ 4.Advanced_Android_Features/Eggtimer/app/src/main/java/com/d_task/asus/eggtimer/MainActivity.java
@@ -1,15 +1,113 @@
 package com.d_task.asus.eggtimer;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+    MediaPlayer honkSound;
+    MediaPlayer tickSound;
+    Boolean counterIsActive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer(){
+
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("GO!");
+        counterIsActive = false;
+
+    }
+
+    public void buttonClicked(View view) {
+
+        if (counterIsActive) {
+
+            resetTimer();
+
+        } else {
+
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("STOP!");
+
+            honkSound = MediaPlayer.create(this, R.raw.airhorn);
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long l) {
+                    updateTimer((int) l / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    honkSound.start();
+                    resetTimer();
+                }
+            }.start();
+        }
+    }
+
+    public void updateTimer ( int secondsLeft) {
+
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+        timerSeekBar.setProgress(secondsLeft);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tickSound = MediaPlayer.create(this, R.raw.tick);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.countdownTextView);
+        goButton = findViewById(R.id.goButton);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                updateTimer(i);
+                tickSound.start();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
