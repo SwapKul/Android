d03633adac45d13f0ea0968d64a4191db7337442
SwapKul <47084735+SwapKul@users.noreply.github.com>
2020-09-23 11:58:26 +0530

    updates

---
+++ Java_tryouts/src/Advance_string_manipulation.java
@@ -0,0 +1,11 @@
+public class Advance_string_manipulation {
+
+    public static void main(String[] args) {
+
+        String myString = "Nick x Sean x Fido x Sara";
+
+        String[] splitstring = myString.split("x");
+
+    }
+
+}
