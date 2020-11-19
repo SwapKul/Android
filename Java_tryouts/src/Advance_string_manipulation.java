836296d33dd0ff94bdd66872e609dfecf800eb39
SwapKul <47084735+SwapKul@users.noreply.github.com>
2020-11-19 19:30:28 +0530

    .

--- Java_tryouts/src/Advance_string_manipulation.java
+++ Java_tryouts/src/Advance_string_manipulation.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+
 public class Advance_string_manipulation {
 
     public static void main(String[] args) {
 
         String myString = "Nick x Sean x Fido x Sara";
 
-        String[] splitstring = myString.split("x");
+        String[] splitstring = myString.split(" x ");
+
+        System.out.println(Arrays.toString(splitstring));
+
+        String river = "Mississippi";
+
+        String[] riverString = river.split("s");
+
+        System.out.println(Arrays.toString(riverString));
 
     }
 
